96af6119ae3b564540c9ff6253c4a6a328d20eed
Michael Robertson <mirob2005@gmail.com>
2013-06-08 17:52:53 -0700

    Added delete operation to Binary Heap

--- ADTs/Binary_Heap_Array_Structure.java
+++ ADTs/Binary_Heap_Array_Structure.java
@@ -6,6 +6,10 @@
  * Binary_Heap_Array_Structure.java
  * 
  * Implements a max heap
+ * Children are at [2i+1] and [2i+2]
+ * Parent is at [floor((i-1)/2)]
+ * where the array goes from 0 to n-1 and the root is at index 0
+ * 
  */
 import java.util.List;
 import java.util.ArrayList;
@@ -34,9 +38,45 @@ class Binary_Heap_Array_Structure<K extends Comparable<K>> {
 			parent = list.get(parentIndex);
 		}
 	}
-	public void delete(K deleteMe){
+	public K delete(){
+		if(this.list.isEmpty()){
+			return null;
+		}
+		K deleted = this.list.get(0);
+		this.list.set(0,this.list.get(this.list.size()-1));
+		this.list.remove(this.list.size()-1);
+		this.heapifyDown(0);
+		return deleted;
 	}
-	public void heapifyDown(){
+	public void heapifyDown(int index){
+		K rightChild = this.getRightChild(index);
+		K leftChild = this.getLeftChild(index);
+		K parent = this.list.get(index);
+		int nextIndex = index;
+		if(rightChild == null && leftChild == null){
+			return;
+		}
+		else if(rightChild == null || leftChild.compareTo(rightChild)>0){
+			if(parent.compareTo(leftChild)<0){
+				this.list.set(index,leftChild);
+				nextIndex = this.getLeftChildIndex(index);
+				this.list.set(nextIndex,parent);
+			}
+			else{
+				return;
+			}
+		}
+		else{
+			if(parent.compareTo(rightChild)<0){
+				this.list.set(index,rightChild);
+				nextIndex = this.getRightChildIndex(index);
+				this.list.set(nextIndex,parent);
+			}
+			else{
+				return;
+			}
+		}
+		this.heapifyDown(nextIndex);
 	}
 	public K peek(){
 		if(!list.isEmpty()){
@@ -115,7 +155,25 @@ class Binary_Heap_Array_Structure<K extends Comparable<K>> {
 		bh.print();
 		bh.insert(0);
 		bh.print();
-		System.out.println("Peek returned: "+bh.peek());
 		bh.traverseBFS();
+		System.out.println("Peek returned: "+bh.peek());
+		System.out.println("Delete Returned: " + bh.delete());
+		bh.print();
+		bh.traverseBFS();
+		
+		Binary_Heap_Array_Structure <String>bhs = new Binary_Heap_Array_Structure<String>();
+		bhs.insert("Three");
+		bhs.print();
+		bhs.insert("ONe");
+		bhs.print();
+		bhs.insert("Four");
+		bhs.print();
+		bhs.insert("Zero");
+		bhs.print();
+		bhs.traverseBFS();
+		System.out.println("Peek returned: "+bhs.peek());
+		System.out.println("Delete Returned: " + bhs.delete());
+		bhs.print();
+		bhs.traverseBFS();
 	}
 }
